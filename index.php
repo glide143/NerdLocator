fa17e241a0528d986dda7856277a8e0aeeedbb76
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-10 19:49:55 +0800

    Add php

---
+++ index.php
@@ -0,0 +1,23 @@
+$json2echo=array(
+                      "speech"=> "",
+                      "messages"=> [array(
+                                       "title"=> 'Oblivion',
+                                       "subtitle"=> 'Oblivion is a SciFi film.',
+                                       "buttons"=> [ array(
+                                                      "text"=> "view film",
+                                                      "postback"=>"https://www.moovrika.com/m/3520"
+                                                  ),
+                                           array(
+                                                      "text"=> "Ask something else",
+                                                      "postback"=>"I want to ask something else"
+                                                  )
+                                            ]
+                                          ,
+                                        "type"=> 1
+                                   )]
+                        );
+
+
+
+      header('Content-Type: application/json');
+      echo json_encode($json2echo, JSON_UNESCAPED_SLASHES);*/
